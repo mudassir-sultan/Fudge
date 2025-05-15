2c8fd67e7e44693031a28cc8f5708b5b6c79c881
MudassarSultan <mudassar.sultan@rentacenter.com>
2025-05-15 22:52:23 +0500

    super admin changes

--- js/all.js
+++ js/all.js
@@ -372,4 +372,25 @@ $(document).ready(function () {
             $("#signInPasswordSuccess").removeClass("d-none");
         }
     });
+
+    // Delete Client List 
+    $('.deleteClient').on('click', function () {
+        Swal.fire ({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+                });
+            }
+        });
+    });
 });
